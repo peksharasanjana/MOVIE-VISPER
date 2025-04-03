824732d700fb5f842e5a6a52fcc965a70abdb509
peksharasanjana <ddmaduwa567@gmail.com>
2025-04-03 14:12:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'BV4kgaIA#tLW_UfR2gVSrzXQlH-A6A-3Q9YwttlRvIRfaqgTdi8g' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'oVIlGQwa#g74sNJKyrChTjQBOk5FMey6vjAPAKQahm1X9q5gCyJ0' : process.env.SESSION_ID, 
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'GiGu6VPwfsuDlHRQlKa6Rn7bWBdtUd0lmXU3' : process.env.GITHUB_AUTH_TOKEN,  //🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.  
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'peksharasanjana' : process.env.GITHUB_USER_NAME, // 🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
